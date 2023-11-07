0109c3f7e16f4c546aa149ca13fa59d2d8c5b65a
Lam Nguyen <lamcnguyen89@gmail.com>
2023-11-07 12:57:26 -0500

    Started on Nearest Neighbor Classifier

--- Assignment_03/02_Nearest_Neighbor_Classification/K_Nearest_Neighbor_Classifier.py
+++
@@ -1,6 +0,0 @@
-"""
-Programming Assignment 3
-
-3. Implement a k-nearest neighbor classifier using pixels as features.
-    a. Test method for k=3,5, and 7 and compute classification accuracy.
-"""
--- Assignment_03/02_Nearest_Neighbor_Classification/Nearest_Neighbor_Classifier.py
+++
@@ -1,8 +0,0 @@
-"""
-Programming Assignment 3
-
-2. Implement Neighbor Classification using pixels as features. Test the method for classification accuracy.
-
-"""
-
-
